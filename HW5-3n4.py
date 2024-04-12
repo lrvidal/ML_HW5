a05592ff54e0524163d23f7a2f471414b572278b
Lucas Ribeiro Vidal <lucas.ribeirovid@gmail.com>
2024-04-12 13:28:31 -0500

    a

--- HW5-3n4.py
+++ HW5-3n4.py
@@ -1,9 +1,29 @@
 import sklearn.model_selection as sklMS
 from sklearn.linear_model import LogisticRegression
 
-def MSE(model, dataset):
-    return sum([(model.predict([item])[0] - item[-1])**2 for item in dataset]) / len(dataset)
+def zeroOneLoss(model, dataset):
+    data = [[0, 0, 0, 0], 0.0]
+    for item in dataset:
+        if model.predict([item])[0] == item[-1]:
+            if item[-1] == 0:
+                data[0][0] += 1
+            else:
+                data[0][1] += 1
+            data[1] += 0
+        else:
+            if item[-1] == 1:
+                data[0][2] += 1
+            else:
+                data[0][3] += 1
+            data[1] += 1
+    data[1] /= len(dataset)
+    return data
 
+def printConfusionMatrix(data):
+    print("       Negative   |   Positive")
+    print("--------------------------------")
+    print("True:   {}      |     {}".format(data[0], data[1]))
+    print("False:  {}        |     {}".format(data[2], data[3]))
 
 input_file = "./cammeo_osmancik.data"
 mapping = {
@@ -44,8 +64,17 @@ for data in validate_data:
     l2regPredicts.append(l2regModel.predict([data])[0])
     noneregPredicts.append(noneregModel.predict([data])[0])
 
-l2regMSE = MSE(l2regModel, validate_data)
-noneregMSE = MSE(noneregModel, validate_data)
+l2data01, l2reg01 = zeroOneLoss(l2regModel, validate_data)
+nonedata01, nonereg01 = zeroOneLoss(noneregModel, validate_data)
 
-print("L2 Regularization MSE: {}".format(l2regMSE))
-print("No Regularization MSE: {}".format(noneregMSE))
+print("L2 Regularization 0-1 Loss: {}".format(l2reg01))
+printConfusionMatrix(l2data01)
+print("No Regularization 0-1 Loss: {}".format(nonereg01))
+printConfusionMatrix(nonedata01)
+
+bestModel = LogisticRegression(penalty=None)
+bestModel.fit(train_data + validate_data, [item[-1] for item in (train_data + validate_data)])
+
+testData01, testReg01 = zeroOneLoss(bestModel, test_data)
+print("Test Data 0-1 Loss: {}".format(testReg01))
+printConfusionMatrix(testData01)
